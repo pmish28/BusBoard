0851d75ef264772e0edf568257a5a4d15318bf67
Preeti Mishra <preeti.mishra@softwire.com>
2025-01-22 12:54:44 +0000

    Added the basic fetch code

--- BusBoard.js
+++ BusBoard.js
@@ -1,6 +1,12 @@
 import fetch from 'node-fetch';
-import dotenv from 'dotenv';
-dotenv.config();
 
 const api_key = process.env.API_KEY
 
+const fetchTfl = async (api_key) => {
+    const response = await fetch(`https://api.tfl.gov.uk/StopPoint/Mode/bus/Disruption`);
+    return(response.status);
+}
+
+console.log(api_key);
+const response = await fetchTfl(api_key);
+console.log(response);
