3d167081f2cffbf33030358a7db6213d59f3c6be
Preeti Mishra <preeti.mishra@softwire.com>
2025-01-22 15:30:47 +0000

    Added the logic to print bus details

--- BusBoard.js
+++ BusBoard.js
@@ -1,17 +1,9 @@
 import fetch from 'node-fetch';
-//import prompt from 'prompt-sync';
 import readline from 'readline-sync';
-//const prompt = require('promp-sync')();
+
 const api_key = process.env.API_KEY
 
-// const fetchTfl = async (api_key) => {
-//     const response = await fetch(`https://api.tfl.gov.uk/StopPoint/Mode/bus/Disruption`);
-//     return(response);
-// }
-// const response = await fetchTfl();
-// console.log(response);
-
-//let busStopCode = "940GZZLUHWE" // Remove this hardcoded for testing
+//let busStopCode = "490G00013535"// "490G00013535" 940GZZLUHWE// Remove this hardcoded value for testing
 let busStopCode = readline.prompt("Please enter bus stop code:");
 
 const fetchTflBuses = async (busStopCode) => {
@@ -19,13 +11,19 @@ const fetchTflBuses = async (busStopCode) => {
         return(response.json());
 }
 const busResponse = await fetchTflBuses(busStopCode);
-// console.log(busResponse);
-
-for(let i =0;i<5;i++)
+console.log(busResponse.length);
+if(busResponse.length > 0)
 {
-    console.log(busResponse[i]["destinationName"] +" " 
-        +busResponse[i]["vehicleId"]+" " 
-        +busResponse[i]["lineName"]+" "
-        +busResponse[i]["timeToStation"]    
-    ); //vehicleId lineName timeToStation
+    for(let i =0;i<5;i++)
+        {
+            console.log(busResponse[i]["destinationName"] +" " 
+                +busResponse[i]["vehicleId"]+" " 
+                +busResponse[i]["lineName"]+" "
+                +busResponse[i]["timeToStation"]    
+            ); 
+        }        
+}
+else
+{
+        console.log("No data present");
 }
