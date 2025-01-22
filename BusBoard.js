b41f81287d62ebae9ed2acdec62c2d5dc81d9ba8
Preeti Mishra <preeti.mishra@softwire.com>
2025-01-22 12:13:27 +0000

    Added gitignoreand readme file

---
+++ BusBoard.js
@@ -0,0 +1,6 @@
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const api_key = process.env.API_KEY
+
---
+++ README.md
@@ -0,0 +1,4 @@
+Create a file called .env in the root of the repository.
+
+Add the line:
+API_KEY=<YourTflApiKey>
